733003fcf58805dca4ccb21c17000fe5c7244670
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-07 10:56:32 -0500

    scratch org updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n OrgPoolPerms
 sfdx force:data:tree:import -p data/Org_Pool__c-Temporary_Pool__c-plan.json
+sfdx force:user:password:generate
 sfdx force:org:open
